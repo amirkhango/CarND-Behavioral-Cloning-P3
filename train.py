319904b3d1a21cab6f7488f13cafde763bf0166e
AamirKhan <kudou@kudoudeMacBook-Pro.local>
2017-11-09 23:57:46 +1100

    commit lenet

--- train.py
+++ train.py
@@ -28,15 +28,24 @@ y_train=np.array(measurements)[:1002]
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Conv2D, MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 ))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Dropout(0.25))
 model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+#model.add(Dropout(0.5))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2,
-	shuffle=True, nb_epoch=5)
+	shuffle=True, nb_epoch=2)
 
 model.save('model.h5')
 
