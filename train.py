566cf534339c4c356094c45b4e6389bc6523466a
Bin Wang <530147789@qq.com>
2017-11-09 12:32:49 +0000

    try 1 layer nn

--- train.py
+++ train.py
@@ -10,8 +10,6 @@ with open('./data/driving_log.csv') as csvfile:
 	for line in reader:
 		lines.append(line)
 
-print(lines[0])
-
 images=[]
 measurements=[]
 
@@ -25,22 +23,20 @@ for line in lines:
 	measurement = float(line[3])
 	measurements.append(measurement)
 
-X_train=np.array(images)
-y_train=np.array(measurements)
-
-print(X_train.shape)
-print(y_train.shape)
+X_train=np.array(images)[:1002]
+y_train=np.array(measurements)[:1002]
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2,
-	shuffle=True, nb_epoch=7)
+	shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
 
