94e8e12cf26657bf445947b7cb1cc753b54ae75a
AamirKhan <kudou@kudoudeMacBook-Pro.local>
2017-11-18 18:54:48 +1100

    finish the generator

--- train.py
+++ train.py
@@ -14,8 +14,9 @@ np.random.seed(1337)  # for reproducibility
 path_model='./MODEL'
 path_log = './log'
 nb_epoch=5
-batch_size=32
+batch_size=2
 hyperparams_name = 'myCNN'
+save_file = os.path.join(path_log, '{}.loss.png'.format(hyperparams_name))
 
 def visual_log(history,save_path):
 	# summarize history for loss
@@ -26,40 +27,52 @@ def visual_log(history,save_path):
 	plt.xlabel('epoch')
 	plt.legend(['train', 'val'], loc='upper left')
 	#plt.show()
-
-	save_file = os.path.join(path_log, '{}.loss.png'.format(hyperparams_name))
+	
 	plt.savefig(save_file)
 	print('Training loss visualizaiton has been saved in :', save_file)
 
-
-def load_data():
-	lines=[]
+def load_samples():
+	samples=[]
 	with open('./data/driving_log.csv') as csvfile:
 		reader = csv.reader(csvfile)
 		for line in reader:
- 			lines.append(line)
+ 			samples.append(line)
 
-	images=[]
-	measurements=[]
+	return samples
 
-	for line in lines:
-		source_path=line[0]
-		#filename = source_path.split('/')[-1]
-		#current_path='./data/IMG/'+ filename
-		current_path='./data/'+source_path
-		image = np.asarray(Image.open(current_path))
-		images.append(image)
+def gen_data(samples, batch_size):
+	
+	#offset = 0 
+	#print('number of samples is:', len(lines))	
+	num_samples = len(samples)
+	while True:
+		for offset in range(0, num_samples, batch_size):
+			
+			images=[]
+			measurements=[]
+			batch_lines = samples[ offset : offset + batch_size]
 
-		measurement = float(line[3])
-		measurements.append(measurement)
-	#X=np.array(images)[:30]
-	#y=np.array(measurements)[:30]
-        
-	X=np.array(images)
-	y=np.array(measurements)
-	np.random.shuffle(X)
-	np.random.shuffle(y)
-	return X, y
+			for line in batch_lines:
+
+				source_path=line[0]
+				filename = source_path.split('/')[-1]
+				current_path='./data/IMG/'+ filename
+				#current_path='./data/'+source_path
+				image = np.asarray(Image.open(current_path))
+				images.append(image)
+
+				measurement = float(line[3])
+				measurements.append(measurement)
+
+		#X=np.array(images)[:30]
+		#y=np.array(measurements)[:30]
+	        
+				X=np.array(images)
+				y=np.array(measurements)
+				np.random.shuffle(X)
+				np.random.shuffle(y)
+
+			yield X, y
 
 def build_model():
 	model = myCNN()
@@ -70,7 +83,14 @@ def build_model():
 def main():
 
 	print("loading data...")
-	X_train, y_train = load_data()
+
+	samples = load_samples()[:20]
+	len_samples = len(samples)
+
+	train_samples = samples[ :-int(len_samples*0.1) ]
+	valid_samples = samples[ -int(len_samples*0.1): ]
+	train_generator = gen_data(train_samples, batch_size)
+	valid_generator = gen_data(valid_samples, batch_size)
 
 	fname_param = os.path.join(path_model, '{}.best.h5'.format(hyperparams_name))
 	
@@ -79,11 +99,14 @@ def main():
 	    fname_param, monitor='val_mse', verbose=0, save_best_only=True, mode='min')
 
 	model = build_model()
-	history = model.fit(X_train, y_train, validation_split=0.1,
-	    shuffle=True,
-	    nb_epoch=nb_epoch,
-	    batch_size=batch_size,
-	    callbacks=[early_stopping, model_checkpoint],)
+
+	history = model.fit_generator(train_generator, 
+		steps_per_epoch = len(train_samples) / batch_size, 
+		validation_data = valid_generator,
+		validation_steps = len(valid_samples) / batch_size,
+	    nb_epoch = nb_epoch,
+	    verbose = 0,
+	    callbacks = [early_stopping, model_checkpoint])
 
 	model.save(os.path.join(path_model, '{}.h5'.format(hyperparams_name)), overwrite=True)
 	print('{} is saved in {}'.format('{}.h5'.format(hyperparams_name), path_model))
@@ -94,7 +117,7 @@ def main():
 	# list all data in history
 	print(history.history.keys())
 
-	visual_log(history=history.history, save_path=path_log)
+	visual_log(history=history.history, save_path=save_file)
 
 
 if __name__ == '__main__':
